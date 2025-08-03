6d0332809b734a246cc73d8e104e8a935d030194
xup60521 <84887662+xup60521@users.noreply.github.com>
2025-08-03 23:31:50 +0800

    not important

--- test.ts
+++
@@ -1,6 +0,0 @@
-import { generateText } from "ai";
-import { input_select_model } from "./utils";
-
-const model = await input_select_model();
-const { text } = await generateText({ model: model.model, prompt: "Hello, world!" });
-console.log(text);
